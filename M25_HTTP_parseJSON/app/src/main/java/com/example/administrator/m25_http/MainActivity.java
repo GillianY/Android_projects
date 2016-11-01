f4edefd84a320dce0fcbedf1bed81baf81b9a64d
Gillian <ginaginayang@gmail.com>
2016-11-01 16:58:17 +0800

    Http ParseJSON ListView
    M25 Http ParseJSON ListView

--- M25_HTTP_parseJSON/app/src/main/java/com/example/administrator/m25_http/MainActivity.java
+++ M25_HTTP_parseJSON/app/src/main/java/com/example/administrator/m25_http/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,6 +22,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     //Data
     private String[] data;
     private int selectedIndex;
+
+    private ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         super.run();
-                        processHttpGetMethod();
+
+                        processHttpGetMethod2();
                     }
                 };
                 t.start();
@@ -78,16 +85,41 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         processHttpPostMethod();
-
                     }
                 }).start();
 
             }
         });
     }
-    private void processJSON(String str)
+    private String[] processJSON(String str)
     {
-
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            JSONArray array = new JSONArray(str);
+            for(int i =0;i<array.length();i++)
+            {
+                JSONObject obj= array.getJSONObject(i);
+                String tmp = ""+  obj.get("id") +":" +obj.get("cate_name");
+                list.add(tmp);
+                Log.i("Gina",tmp);
+            }
+            return list.toArray(new String[0]);
+            /*
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String[] data2 = list.toArray(new String[0]);
+                    Log.i("Gina",data2.toString());
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1, data2);
+                    listview.setAdapter(adapter);
+                }
+            });
+            */
+        }catch(Exception e)
+        {
+            Log.i("Gina",e.toString());
+        }
+        return null;
     }
 
 
@@ -134,11 +166,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void processHttpGetMethod2( ) {
+        HttpClient httpclient = new DefaultHttpClient();
+        String url = "https://www.dropbox.com/s/7z1530esmb6gtlq/myjson.txt?dl=1";// //("http://ikenapp.appspot.com/json/getCategory.jsp");
+        try {
+            HttpResponse response = httpclient.execute(new HttpGet(url));
+            String  str = EntityUtils.toString(response.getEntity(),"utf-8");
+            final String[] data = processJSON(str);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                 ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,data);
+                 listview.setAdapter(adapter);
+                }
+            });
+        } catch (Exception e) {
+            Log.e("GET", e.getMessage());
+        }
+    }
+
     private void initView() {
         spinner = (Spinner) findViewById(R.id.spinner);
         get = (Button) findViewById(R.id.btn_get);
         post = (Button) findViewById(R.id.btn_post);
-        display = (TextView) findViewById(R.id.tv_display);
+       // display = (TextView) findViewById(R.id.tv_display);
+        listview =(ListView) findViewById(R.id.lv_list);
 
         data = new String[]{"1","2","3","4","5","6"};
         ArrayAdapter adapter = new ArrayAdapter(this,
